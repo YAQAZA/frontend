d57395ef097313e6cd134c157d513378a87dde2f
yousef-showman <123179662+yousef-showman@users.noreply.github.com>
2026-05-12 17:06:53 +0300

    add the yolo model

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -3,4 +3,5 @@ class AppAssets {
 
   static const String logoImage = 'assets/images/logo.png';
   static const String drowsinessPath = 'assets/models/gru_unrolled.tflite';
+  static const String distractionPath = 'assets/models/YOLO.tflite';
 }
